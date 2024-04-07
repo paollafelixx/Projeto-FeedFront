a447a8df454c1d95759e256f1ea3a9efee7f09ce
Paolla Felix <127573307+paollafelixx@users.noreply.github.com>
2024-04-06 22:10:22 -0300

    README.me

---
+++ feedfrent/src/main/java/br/com/fiap/feedfrent/FeedfrentApplication.java
@@ -0,0 +1,13 @@
+package br.com.fiap.feedfrent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FeedfrentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FeedfrentApplication.class, args);
+	}
+
+}
---
+++ feedfrent/src/test/java/br/com/fiap/feedfrent/FeedfrentApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fiap.feedfrent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FeedfrentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
